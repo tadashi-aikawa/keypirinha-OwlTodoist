aa4d68efad5c6a6560e10949e15728ec40c7a8b1
tadashi-aikawa <syou.maman@gmail.com>
2020-02-20 19:35:38 +0900

    Add support for Google calendar instead of Slack notifications

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import re
 
@@ -8,71 +9,50 @@ import keypirinha as kp
 import keypirinha_util as kpu
 
 """
-時刻表データ転送高速化の今後の予定決め
-Dec 12 from 4:00 PM to 4:30 PM at 2F
+Googleカレンダーの予定からコピペをして..
 
-:dancer:【確保】
-Tomorrow from 11:30 AM to 12:00 PM
+MTG のタイトル 2月 25日 (火曜日)⋅15:00～16:00
+  ↓
+以下のようなTodoistタスクに変換する
+  ↓
+:calendar: MTG のタイトル (15:00-16:00) 2/25 @30分 
 """
 
-
-def convert24h(timestr: str) -> str:
-    """ 2:00 PM => 14:00
-        2:00 AM => 2:00
-    """
-    return datetime.strptime(timestr, "%I:%M %p").strftime("%H:%M")
-
+non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), '')
 
 def behindMinutes(start: str, end: str) -> int:
     """
-    @param start 14:56 PM
-    @param end 15:30 PM
+    @param start 14:56
+    @param end 15:30
     """
-    delta = datetime.strptime(end, "%I:%M %p") - \
-        datetime.strptime(start, "%I:%M %p")
+    delta = datetime.strptime(end, "%H:%M") - datetime.strptime(start, "%H:%M")
     return int(delta.seconds / 60)
 
 
-def convert_yy_mm_dd(datestr: str) -> str:
-    """ If today is 2018-12-10
-        Dec 12 => 2018-12-12
-        Jan 7  => 2019-01-07
-    """
-    if datestr == "Today":
-        d = datetime.today()
-    elif datestr == "Tomorrow":
-        d = datetime.today() + timedelta(days=1)
-    else:
-        d = datetime.strptime(f'{datestr} {datetime.now().year}', "%b %d %Y")
-        if d < datetime.now():
-            d = datetime(d.year + 1, d.month, d.day)
-    return d.strftime("%Y-%m-%d")
-
-
-def convert2todoist(calendar_summary: str) -> str:
+def convert2todoist(calendar_summary: str, info) -> str:
     m = re.match(
-        r'(?P<title>.+) (?P<date>((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d\d?|Today|Tomorrow)) from (?P<start>\d\d?:\d\d? [AP]M) to (?P<end>\d\d?:\d\d? [AP]M).*',
+        r"(?P<title>.+) (?P<month>\d\d?)月 (?P<day>\d\d?)日 \(.曜日\)⋅(?P<start>\d\d?:\d\d?)～(?P<end>\d\d?:\d\d?)",
         calendar_summary,
     )
     if not m:
         return None
-    return f":calendar: {m['title']} ({convert24h(m['start'])}-{convert24h(m['end'])}) {convert_yy_mm_dd(m['date'])} @{behindMinutes(m['start'], m['end'])}分"
+    return f":calendar: {m['title']} ({m['start']}-{m['end']}) {m['month']}/{m['day']} @{behindMinutes(m['start'], m['end'])}分".translate(non_bmp_map)
 
 
 class OwlTodoist(kp.Plugin):
     ITEMCAT_RESULT = kp.ItemCategory.USER_BASE + 1
 
     def on_start(self):
-        self.info("Welcome to OwlTodoist!!")
+        self.info("Welcome to OwlTodoist!!!!!!!")
 
     def on_catalog(self):
         catalog = self.create_item(
             category=kp.ItemCategory.REFERENCE,
-            label='OwlTodoist: Covert to todoist contents',
-            short_desc='Convert to todoist',
-            target='Convert your input string to todoist contents.',
+            label="OwlTodoist: Covert to todoist contents",
+            short_desc="Convert to todoist",
+            target="Convert your input string to todoist contents.",
             args_hint=kp.ItemArgsHint.REQUIRED,
-            hit_hint=kp.ItemHitHint.IGNORE
+            hit_hint=kp.ItemHitHint.IGNORE,
         )
         self.set_catalog([catalog])
 
@@ -80,21 +60,23 @@ class OwlTodoist(kp.Plugin):
         if not items_chain or not user_input:
             return
 
-        todoist_str: str = convert2todoist(user_input)
+        todoist_str: str = convert2todoist(user_input, self.info)
+        self.info(todoist_str)
         if not todoist_str:
             return
 
         suggestion = self.create_item(
             category=self.ITEMCAT_RESULT,
             label=todoist_str,
-            short_desc='Copy this string',
+            short_desc="Copy this string",
             target=todoist_str,
             args_hint=kp.ItemArgsHint.FORBIDDEN,
-            hit_hint=kp.ItemHitHint.IGNORE
+            hit_hint=kp.ItemHitHint.IGNORE,
         )
         self.set_suggestions([suggestion], kp.Match.ANY, kp.Sort.NONE)
 
     def on_execute(self, item, action):
-        self.info('execute')
+        self.info("execute")
         if item and item.category() == self.ITEMCAT_RESULT:
             kpu.set_clipboard(item.target())
+
